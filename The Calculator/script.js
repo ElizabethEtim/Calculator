1398325e19a1394ac59fe9e1d380356c0536b4b4
ElizabethEtim <168894355+ElizabethEtim@users.noreply.github.com>
2024-08-08 22:06:48 +0100

    Add files via upload
    new

--- The Calculator/script.js
+++ The Calculator/script.js
@@ -19,3 +19,74 @@ function calculateResult() {
         display.innerText = 'Error';
     }
 }
+
+/*let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null
+let shouldResetScreen = false
+let currentOperationScreen =''
+
+function appendToDisplay(value) {
+    if (currentOperation.textContent === '0')
+    currentOperation.textContent += value
+  }
+
+
+function setOperation(operator) {
+    if (currentOperation !== null) evaluate()
+    firstOperand = currentOperationScreen.textContent
+    currentOperation = operator
+    shouldResetScreen = true
+  }*/
+
+  
+  function add(a, b) {
+    return a + b
+  }
+  
+  function substract(a, b) {
+    return a - b
+  }
+  
+  function multiply(a, b) {
+    return a * b
+  }
+  
+  function divide(a, b) {
+    return a / b
+  }
+
+  function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
+    switch (operator) {
+      case '+':
+        return add(a, b)
+      case '−':
+        return substract(a, b)
+      case '×':
+        return multiply(a, b)
+      case '÷':
+        if (b === 0) return null
+        else return divide(a, b)
+      default:
+        return null
+    }
+  }
+
+  /*function calculateResult(){
+   
+    let operator;
+    let result;
+    
+    if(operator == "+"){
+        result = (a+b);
+    }if(operator == "-"){
+        result = (a-b);
+    }if(operator == "*"){
+        result = (a*b);
+    }if(operator == "/"){
+        result = (a/b);
+    }
+    display.innerText = result
+}*/
